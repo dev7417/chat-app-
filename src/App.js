8f351a5d383b4cdb51d4047c03f5b7042ec44327
Dev <gangwardev50@gmail.com>
2022-11-28 11:49:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/Navbar';
+
+const style = {
+  appContainer:`max-w-[728px] mx-auto text-center`,
+  sectionContainer:`flex flex-col h-[90vh] bg-grey-100 mt-10 shadow-xl border relative`
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div className={style.appContainer}>
+          <section className={style.sectionContainer}>
+            <Navbar/>
+          </section>
+        </div>
+    </>
   );
 }
 
